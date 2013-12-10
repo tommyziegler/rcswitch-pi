f41127ac50737223dc321e94ee1acb76c36eb780
Tommy Ziegler <me@tommyziegler.com>
2013-12-10 17:48:43 +0100

    Add missing wiringPiSetup()

--- sendElro.cpp
+++ sendElro.cpp
@@ -6,6 +6,9 @@
 
 int main(int argc, const char * argv[])
 {
+    if (wiringPiSetup () == -1)
+        return 1;
+    
     SwitchElro((char*)argv[1], (char*)argv[2], (bool)atoi(argv[3]));
     return 0;
 }
--- sendRev.cpp
+++ sendRev.cpp
@@ -6,6 +6,9 @@
 
 int main(int argc, const char * argv[])
 {
+    if (wiringPiSetup () == -1)
+        return 1;
+    
     SwitchRev((char*)argv[1], (int)atoi(argv[2]), (bool)atoi(argv[3]));
     return 0;
 }
